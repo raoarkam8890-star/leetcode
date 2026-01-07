f99518dc16ed9852884a0151fce0a980918e9dfb
raoarkam8890-star <raoarkam8890@gmail.com>
2026-01-07 22:05:18 +0500

    Time: 0 ms (100.00%) | Memory: 42.6 MB (78.80%) - LeetSync

---
+++ 225-implement-stack-using-queues/implement-stack-using-queues.java
@@ -0,0 +1,27 @@
+class MyStack {
+
+    Queue<Integer> q = new LinkedList<>();
+
+    public MyStack() {}
+
+    public void push(int x) {
+        q.add(x);
+        int size = q.size();
+        while (size > 1) {
+            q.add(q.remove());
+            size--;
+        }
+    }
+
+    public int pop() {
+        return q.remove();
+    }
+
+    public int top() {
+        return q.peek();
+    }
+
+    public boolean empty() {
+        return q.isEmpty();
+    }
+}
